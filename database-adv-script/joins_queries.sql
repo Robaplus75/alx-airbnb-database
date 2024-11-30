7375575f2f2d26640241b34dcf8cc36e6d1da041
robel <robelwon75@gmail.com>
2024-11-30 21:46:29 +0300

    added order by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,3 @@
 SELECT * FROM Booking INNER JOIN User ON Booking.user_id = User.user_id;
-SELECT * From Property LEFT JOIN Review ON Property.property_id = Review.property_id;
+SELECT * From Property LEFT JOIN Review ON Property.property_id = Review.property_id ORDER BY Property.name;
 SELECT * FROM Booking FULL OUTER JOIN User ON Booking.user_id = User.user_id;
