692bb88a9b3da80710c7bb1a2b4fc7f68ce2f6f6
robel <robelwon75@gmail.com>
2024-12-01 13:58:03 +0300

    index performance

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,5 @@
+EXPLAIN ANALYZE SELECT * FROM Booking b INNER JOIN User u ON b.user_id = u.user_id INNER JOIN Property p ON b.property_id = p.property_id WHERE b.status = 'confirmed' AND b.created_at >= '2024-01-01';
+
 -- AFTER ANALYZING WITH ( EXPLAIN AND ANALYZE ) THE FOLLOWING COLUMNS NEEDS TO BE INDEXED
 
 CREATE INDEX idx_booking_user_id ON Booking(user_id);
@@ -7,3 +9,5 @@ CREATE INDEX idx_user_email ON User(email);
 CREATE INDEX idx_booking_created_at ON Booking(created_at);
 CREATE INDEX idx_property_created_at ON Property(created_at);
 CREATE INDEX idx_booking_status ON Booking(status);
+
+EXPLAIN ANALYZE SELECT * FROM Booking b INNER JOIN User u ON b.user_id = u.user_id INNER JOIN Property p ON b.property_id = p.property_id WHERE b.status = 'confirmed' AND b.created_at >= '2024-01-01';
