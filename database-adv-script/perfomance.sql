54815b92dbbbde3f9d6d8c13879591c79acec35c
robel <robelwon75@gmail.com>
2024-12-01 14:23:30 +0300

    optimization report

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,1 +1,3 @@
-SELECT * FROM Booking JOIN User ON User.user_id=Booking.user_id JOIN Payment on Booking.booking_id=Payment.booking_id;
+SELECT * FROM Booking JOIN User ON User.user_id=Booking.user_id JOIN Payment on Booking.booking_id=Payment.booking_id WHERE Payment.amount > 0 AND Payment.amount < 10000000.00;
+
+EXPLAIN ANALYZE SELECT * FROM Booking JOIN User ON User.user_id=Booking.user_id JOIN Payment on Booking.booking_id=Payment.booking_id WHERE Payment.amount > 0 AND Payment.amount < 10000000.00;
